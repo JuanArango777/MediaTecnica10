946860844a1f5dcd3bda9ac4db8e89b74316e1af
Juan 🐴👍🏼 <vergaraarangojuanfernando2007@gmail.com>
2024-05-16 15:15:25 -0500

    Demogorgon

---
+++ Demogorgon.cs
@@ -0,0 +1,22 @@
+/******************************************************************************
+Autor: Juan Arango y andresito martinez
+Fecha creacion: 2024-05.16
+Descripcion:Los personajes de Stranger Things necesitan
+un programa en C# para registrar los encuentros
+con criaturas del mundo como el Demogorgon.
+Completa el espacio en blanco para imprimir
+el nombre de la criatura en la consola.
+ 
+
+*******************************************************************************/
+using System;
+class HelloWorld
+{
+  static void Main()
+  {
+      string criatura = "Demogorgon";
+      
+      Console.WriteLine("el nombre de la criatura es: " + criatura);
+  }
+  
+}
